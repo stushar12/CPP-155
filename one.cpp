deca27114189cfc6f57db77f3b657d03815d12f1
Tushar Saxena <stushar345@gmail.com>
2021-03-28 12:47:22 +0530

    Update one.cpp

--- one.cpp
+++ one.cpp
@@ -3,33 +3,36 @@ using namespace std;
 
 int main()
 {
-	int arr[]={2,3,3,1,3,2};
-	int size=6;
+    int arr[]={2,3,3,1,3};
+    int size=5;
 
-	int majority=arr[0];
-	int count=1;
+    int majority=arr[0];
+    int count=1;
 
-	for(int i=1;i<size;i++)
-	{
-		if(arr[i]==majority)
-		count++;
-		else
-		{
-			count--;
-			majority=arr[i];
-			count=1;
+    for(int i=1;i<size;i++)
+    {
+        if(arr[i]==majority)
+        count++;
+        else
+        count--;
 
-		}
-	}
-	 count=0;
-	for(int i=0;i<size;i++)
-	{
-		if(arr[i]==majority)
-		count++;
-	}
+    if(count==0)
+    {
+        majority=arr[i];
+        count=1;
+    }
+    }
 
-	if(count>(size/2))
-	cout<<majority;
-	else
-	cout<<"No element exists";
+
+     count=0;
+    for(int i=0;i<size;i++)
+    {
+        if(arr[i]==majority)
+        count++;
+    }
+
+    if(count>(size/2))
+    cout<<majority;
+    else
+    cout<<"No element exists";
 }
